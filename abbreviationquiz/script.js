6c2a0c9e54549f70f73d40775e821427b119d640
Oliver Christensen <ollie.a.c@outlook.com>
2024-11-14 19:28:14 -0700

    minor changes

---
+++ abbreviationquiz/script.js
@@ -0,0 +1,14 @@
+//constants for input buttons
+
+
+//listen for keyup on input
+//iterate through array of abbreviations
+//if there is a match, display in found list and display # of found
+//if # = array.length, end game
+
+//listen to give up button
+//end game
+
+//end game function
+//disable textbox
+//
